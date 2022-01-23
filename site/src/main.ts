eb2bfc4162b744164dd786d2b6add3b177a99ab7
Emir Guvenni <contact@emirguvenni.com>
2022-01-24 01:08:34 +0300

    Added the main script

---
+++ site/src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "index.css";
+
+const app = createApp(App);
+
+// Vue Fullpage
+// @ts-ignore: Missing type declarations.
+import VueFullPage from "vue-fullpage.js";
+app.use(VueFullPage);
+
+// Mount the app
+app.mount("#app");
