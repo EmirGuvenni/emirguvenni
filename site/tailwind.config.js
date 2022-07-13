667f15ed279d91bc6f86856f50a93021dd1e0d49
Emir Guvenni <contact@emirguvenni.com>
2022-07-13 23:48:00 +0300

    Updated the tailwind config

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,25 +1,14 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-      red: colors.red,
-      yellow: colors.yellow,
-      green: colors.green,
-      blue: colors.blue,
-      indigo: colors.indigo,
-      purple: colors.purple,
-      pink: colors.pink
-    },
+    colors,
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
