5d13e311f0f9f3caeb0353a3043bbfd67c9e39eb
Retux <retux@MacBook-Air.local>
2026-05-14 12:36:38 +0300

    fix(ui/badge): text color in dark mode

--- web/components/ui/badge.tsx
+++ web/components/ui/badge.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export function Badge({ children, className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
-      className={`w-fit rounded-md bg-gray-100 px-3 py-1 text-sm font-semibold ${className}`}
+      className={`text-primary-foreground w-fit rounded-md bg-gray-100 px-3 py-1 text-sm font-semibold ${className}`}
       {...props}
     >
       {children}
